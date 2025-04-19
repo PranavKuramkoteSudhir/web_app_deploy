85580e7c17bec05776e00a683aad13be0aca30e2
PranavKuramkoteSudhir <pranavkuramkotesudhir@gmail.com>
2025-04-18 23:41:12 -0400

    Changed cookie secure to True

--- routes/auth.js
+++ routes/auth.js
@@ -48,8 +48,8 @@ router.post('/login', async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false, // set true in production with HTTPS
-      sameSite: 'lax',
+      secure: True,
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
